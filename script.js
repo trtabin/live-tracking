9d8f583246b4ed725699ddc14ba998238e05537f
Tahmidur Rahman Tabin <trtabin@gmail.com>
2023-10-07 11:42:37 +0600

    Add the location of all devices in the map

--- script.js
+++ script.js
@@ -7,27 +7,33 @@ L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 // Initialize an empty array to store user-provided marker coordinates
 var userMarkers = [];
 
-// Function to add a marker based on user input
-function addMarker() {
-  var latInput = parseFloat(prompt("Enter latitude:"));
-  var lonInput = parseFloat(prompt("Enter longitude:"));
+var firebaseConfig = {
+  apiKey: "AIzaSyBKvVZeimn-iqb38WiqtseKzlFFKjeHQ5c",
+  authDomain: "hydroquotracker-new.firebaseapp.com",
+  projectId: "hydroquotracker-new",
+  storageBucket: "hydroquotracker-new.appspot.com",
+  messagingSenderId: "604958695388",
+  appId: "1:604958695388:web:eaf01e4e1bf1ddf3e23919",
+  measurementId: "G-GSNTDC6PQN",
+  databaseURL: "https://hydroquotracker-new-default-rtdb.firebaseio.com/"
+};
 
-  if (!isNaN(latInput) && !isNaN(lonInput)) {
-      // Create a marker and add it to the map
-      var marker = L.marker([latInput, lonInput]).addTo(map);
+firebase.initializeApp(firebaseConfig);
+
+
+
+firebase.database().ref().on("value", function (snapshot) {
+  console.log();
+  Object.entries(snapshot.val().markers).forEach((entry) => {
+      const [key, value] = entry;
+      console.log(`${key}: ${value.lat} ${value.log}`);
+
+      var marker = L.marker([`${value.lat}`, `${value.log}`]).addTo(map);
 
       // Add the marker to the userMarkers array
       userMarkers.push(marker);
+  });
+}, function (error) {
+  console.log("Error: " + error.code);
+});
 
-      // Center the map to the user's latest marker
-      map.panTo([latInput, lonInput]);
-
-      // Optionally, you can display all user markers in the console
-      console.log("User markers:", userMarkers);
-  } else {
-      alert("Invalid input. Please enter valid latitude and longitude.");
-  }
-}
-
-// Add a click event listener to the "Add Marker" button
-document.getElementById("addMarkerButton").addEventListener("click", addMarker);
